8552cc70f4a1f5d071275890198da4bf72f23e11
dbc-apprentice <apprentice@devbootcamp.com>
2013-06-04 19:47:38 -0500

    finished category and post pages with links

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -1,6 +1,23 @@
 get '/' do
-  #Run some ruby method that populates a list
-  @populate = Category.populate
+  @categories = Category.all
 
   erb :index
+end
+
+get '/:category' do
+  @category_id = params[:category]
+  @category = Category.where(:id => @category_id).first
+  redirect '/' if @category == nil
+  @posts = Post.where(:category_id => @category_id)
+  erb :category_page
+end
+
+get '/:category/:post' do
+  @post = Post.where(:id => params[:post]).first
+
+  erb :post_page
+end
+
+post '/post' do
+  erb :create_post_page
 end
--- app/models/category.rb
+++ app/models/category.rb
@@ -1,11 +1,16 @@
 class Category < ActiveRecord::Base
   # Remember to create a migration!
   has_many :posts, :dependent => :destroy
+  @@array_of_names = []
 
   #class method that populates a list
-  def self.populate
-    array_of_names = []
-    self.all.each { |category| array_of_names << category.name }
-    array_of_names
+  def self.get_names
+    @@array_of_names = []
+    self.all.each { |category| @@array_of_names << category.name }
+    @@array_of_names
+  end
+
+  def self.get_category(object)
+    object.id
   end
 end
